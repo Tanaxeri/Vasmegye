f95c2f3d50163452e0aba6ab5980b658df1e3a9a
Tanaxeri <tamasgamer@gmail.hu>
2022-10-10 09:06:34 +0200

    Add project files.

---
+++ Vasmegye.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vasmegye
+{
+    class Vasmegye
+    {
+        public List<adatok> adat = new List<adatok>();
+        static void CdvEll()
+        {
+
+
+
+            return ;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nA program kezdődik...");
+
+            /*2.feladat*/
+            Console.WriteLine("\n2. feladat: Adatok beolvasása, tárolása");
+            StreamReader sr = new StreamReader("vas.txt" ,Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+
+                adat.Add(new adatok(sr.ReadLine()));
+
+            }
+            sr.Close();
+            /*4.feladat*/
+            Console.WriteLine("\n4. feldat: Ellenőrzés");
+            CdvEll();
+
+            /*5.feladat*/
+            Console.WriteLine("\nVasmegyében a vizsgált évek alatt");
+
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ adatok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasmegye
+{
+    class adatok
+    {
+
+        public int m;
+        public int eehhnn;
+        public int sss;
+        public int k;
+
+        public adatok(int m, int eehhnn, int sss, int k)
+        {
+            this.m = m;
+            this.eehhnn = eehhnn;
+            this.sss = sss;
+            this.k = k;
+        }
+    }
+}
